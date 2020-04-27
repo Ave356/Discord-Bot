a669fd16eadd8d74dc6e2f5f7cc6873e6a5d0644
Ave356 <19156864+Ave356@users.noreply.github.com>
2020-04-27 13:37:12 +0000

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -1,10 +1,20 @@
 import discord
+import os
 from discord.ext import commands
 
-TOKEN = '' # Place bot token here
 client = commands.Bot(command_prefix = '!')
-client = discord.Client()
+TOKEN = 'NjQzMjgzNjY2MDQ2Mjg3OTAy.XplYiQ.MoJhAvFJvoyoH-G5HchHpBLueDE' # Place bot token here
 
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
 
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 
 client.run(TOKEN)
