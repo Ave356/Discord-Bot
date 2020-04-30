d4a4ddca741742963473181d5827a4292ce3710a
Ave356 <19156864+Ave356@users.noreply.github.com>
2020-04-30 12:37:56 +0000

    Added cog loaded/unload chat message

--- bot.py
+++ bot.py
@@ -5,13 +5,19 @@ from discord.ext import commands
 client = commands.Bot(command_prefix = '!')
 TOKEN = '' # Place bot token here
 
+#TODO add error notfication that a cog was already loaded/unloaded
+
 @client.command()
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
+    await ctx.send(f'extension "{extension}" loaded')
+
 
 @client.command()
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
+    await ctx.send(f'extension "{extension}" unloaded')
+
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
