c3659dc9c6f7bd35b38cd9fed5d47dd66e0ccb20
Ave356 <19156864+Ave356@users.noreply.github.com>
2020-05-08 21:30:31 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='Discord-Bot',
+    author="Ave356",
+    author_email=".",
+    url="https://github.com/Ave356/Discord-Bot",
+    version='0.1dev',
+    packages=['Discord-Bot',],
+    license='MIT License',
+    long_description=open('README').read(),
+    platforms="Platform agnostic",
+    install_requires=[
+        'discord==1.0.1',
+        'discord.py==1.3.3',
+        'discordbot.py==0.2.3a3',
+        'youtube-dl==2020.5.8'
+    ]
+)
