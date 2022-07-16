decc5dc4592005e8eb175067ba7f7ad06eabc7bb
Ave356 <19156864+Ave356@users.noreply.github.com>
2022-07-16 11:19:05 -0400

    Update audio_player.py

--- Discord-Bot/cogs/audio_player.py
+++ Discord-Bot/cogs/audio_player.py
@@ -1,13 +1,15 @@
 import os
 
 import discord
-import youtube_dl
+from yt_dlp import YoutubeDL
 from discord.ext import commands
 from discord.utils import get
+
 
 class AudioPlayer(commands.Cog):
     def __init__(self, client):
         self.client = client
+
 
         @client.command(pass_context=True)
         async def join(ctx):
@@ -19,9 +21,7 @@ class AudioPlayer(commands.Cog):
                 await voice.move_to(channel)
             else:
                 voice = await channel.connect()
-                print(f"the bot has connected to {channel}\n")
-
-            await ctx.send(f"joined {channel}")
+                print(f"Bot has connected to {channel}\n")
 
         
         @client.command(pass_context=True)
@@ -32,7 +32,6 @@ class AudioPlayer(commands.Cog):
             if voice and voice.is_connected():
                 await voice.disconnect()
                 print(f"Bot has disconnected from {channel}")
-                await ctx.send(f"Left {channel}")
             else:
                 print("Bot isn't in a channel")
                 await ctx.send("I am not in a channel")
@@ -49,22 +48,18 @@ class AudioPlayer(commands.Cog):
             else:
                 voice = await channel.connect()
                 print(f"the bot has connected to {channel}\n")
-
-            await ctx.send(f"joined {channel}")
+                        
             
-            
-            
-            audio_file = os.path.isfile("audio.mp3")
+            audio_file = os.path.isfile("audio.flac")
             try:
                 if audio_file:
-                    os.remove("audio.mp3")
+                    os.remove("audio.flac")
                     print("Removed audio file")
             except PermissionError:
                 print("Cannot delete audio file: it is being played")
                 await ctx.send("Error: audio playing")
                 return
             
-            await ctx.send("downloading audio")
             
             voice = get(client.voice_clients, guild=ctx.guild)
             
@@ -74,29 +69,28 @@ class AudioPlayer(commands.Cog):
                 'call_home':'false',
                 'postprocessors': [{
                     'key': 'FFmpegExtractAudio',
-                    'preferredcodec': 'mp3',
-                    'preferredquality': '192', # 192 is the default audio quality. Change depending on your setup. https://github.com/ytdl-org/youtube-dl/blob/master/youtube_dl/YoutubeDL.py#L128-L278
+                    'preferredcodec': 'flac',
+                    'preferredquality': '142', # 192 is the default audio quality. Change depending on your setup. https://github.com/ytdl-org/youtube-dl/blob/master/youtube_dl/YoutubeDL.py#L128-L278
                 }],
             }
 
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            with YoutubeDL(ydl_opts) as ydl:
                 print("Downloading audio\n")
                 ydl.download([url])
 
             for file in os.listdir("./"):
-                if file.endswith(".mp3"):
+                if file.endswith(".flac"):
                     name = file
                     print(f"Renamed the file: {file}")
-                    os.rename(file, "audio.mp3")
+                    os.rename(file, "audio.flac")
                 
-            voice.play(discord.FFmpegPCMAudio("audio.mp3"), after=lambda e: print(f"{name} has finished playing"))
+            voice.play(discord.FFmpegPCMAudio("audio.flac"), after=lambda e: print(f"{name} has finished playing"))
             voice.source = discord.PCMVolumeTransformer(voice.source)
             voice.source.volume = 0.3 # Keep at 0.3 or lower else it becomes too loud
 
             nname = name.rsplit("-", 2)
             await ctx.send(f"Playing: {nname[0]}")
             print("Playing\n")
-
 
 
         @client.command(pass_context=True)
@@ -123,6 +117,9 @@ class AudioPlayer(commands.Cog):
                 print("Stopping")
                 voice.stop()
                 await ctx.send('Stopping')
+            if os.path.exists("audio.flac"):
+                os.remove("audio.flac")
+                print("removing audio.flac")
 
 def setup(client):
     client.add_cog(AudioPlayer(client))
